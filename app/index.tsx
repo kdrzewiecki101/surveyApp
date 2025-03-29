a2e5465ca2f7ba5d1caccc8fb96875c509c0129f
unknown <k.drzewiecki101@gmail.com>
2025-03-29 10:24:00 +0100

    app initialization

---
+++ app/index.tsx
@@ -0,0 +1,17 @@
+import { Text, View, Image } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Image source={require('../assets/images/react-logo.png')} />
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
+
